c6c77d514d33e94224ecd12f2f9dd07253895c15
Sundays12 <oliviamboya2@gmail.com>
2023-04-27 09:25:53 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isupper - Checks for upper case charachter
+ * @c: Character to be checked
+ *
+ * Return: on Success 1
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c >= 90)
+	
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
