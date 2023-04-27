7ff9811d281450681bb3584c64100e5e78ef409b
Sundays12 <oliviamboya2@gmail.com>
2023-04-27 00:05:25 -0700

    main c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests the function that prints positive or negative interger
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
