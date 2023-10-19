12817b8ece20dd3b4fb8f90866c8a446872c02c7
Sundays12 <128959615+Sundays12@users.noreply.github.com>
2023-10-19 11:36:56 +0300

    Update 0-puts_recursion.c
    Deletion of code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,1 @@
-#include "main.h"
 
-/**
- * _puts_recursion - prints a string,
- * @s: string
- * Returns: zero
- */
-void _puts_recursion(char *s)
-{
-	if (*s != '\0')
-	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
-}
