48540c23abd8e33b3d352fb11b3b7213b8b9b424
Sundays12 <oliviamboya2@gmail.com>
2023-12-11 15:20:43 -0800

    putchar file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ * Auth: Oli
+ */
+
+int main (int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = _atoi(argv[1]);
+	b = _atoi(argv[2]);
+	printf("%d\n", a * b);
+	return (0);
+}
