bd82f7140659b6b583b708a658da5816085a8c99
Sundays12 <oliviamboya2@gmail.com>
2023-12-11 17:41:44 -0800

    allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ * Auth: Oli
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
